627221b24ab575b18508902a691d8f05a49bd468
unknown <martin.arpin@outlook.com>
2021-08-16 09:40:28 -0700

    Updates

---
+++ precache-manifest.165f191f6c53fafbd458482a12df0f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3603590673a11d68229849b37ba1db17",
+    "url": "/React-Weather/index.html"
+  },
+  {
+    "revision": "62cfc04ddeec9adda681",
+    "url": "/React-Weather/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "62cfc04ddeec9adda681",
+    "url": "/React-Weather/static/js/2.7e0762ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather/static/js/2.7e0762ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0214bc3b81e7ec8351e",
+    "url": "/React-Weather/static/js/main.9b6d2c84.chunk.js"
+  },
+  {
+    "revision": "0c4561f6f6e3632b0d84",
+    "url": "/React-Weather/static/js/runtime-main.782ab875.js"
+  }
+]);
